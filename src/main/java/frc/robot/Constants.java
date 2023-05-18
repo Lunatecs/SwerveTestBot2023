bba88539ed24ebe09d0d2ae236db424020bb159d
Sujay Jukalkar <sujukalkar@gmail.com>
2023-05-18 19:47:09 -0400

    Created DrivetrainSubsystem and Constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    public final static class DrivetrainConstants {
+        public static final int LEFT_FRONT_DRIVE = 0;//TODO: Update CAN values of drive & turning motors
+        public static final int LEFT_FRONT_TURN = 0;
+        public static final int RIGHT_FRONT_DRIVE = 0;
+        public static final int RIGHT_FRONT_TURN = 0;
+        public static final int LEFT_BACK_DRIVE = 0;
+        public static final int LEFT_BACK_TURN = 0;
+        public static final int RIGHT_BACK_DRIVE = 0;
+        public static final int RIGHT_BACK_TURN = 0;
+        public static final int PIGEON = 0;
+    }
+}
--- src/main/java/frc/robot/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/DrivetrainSubsystem.java
@@ -4,16 +4,76 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
   /** Creates a new DrivetrainSubsystem. */
   public static final double maxSpeed = 3.0; // 3 meters per second
   public static final double maxAngularSpeed = Math.PI; // 1/2 rotation per second
 
-  
+  private final Translation2d frontLeftLocation = new Translation2d(0.381, 0.381);
+  private final Translation2d frontRightLocation = new Translation2d(0.381, -0.381);
+  private final Translation2d backLeftLocation = new Translation2d(-0.381, 0.381);
+  private final Translation2d backRightLocation = new Translation2d(-0.381, -0.381);
 
-  public DrivetrainSubsystem() {}
+  private final SwerveModule frontLeft = new SwerveModule(DrivetrainConstants.LEFT_FRONT_DRIVE, DrivetrainConstants.LEFT_FRONT_TURN);
+  private final SwerveModule frontRight = new SwerveModule(DrivetrainConstants.RIGHT_FRONT_DRIVE, DrivetrainConstants.RIGHT_FRONT_TURN);
+  private final SwerveModule backLeft = new SwerveModule(DrivetrainConstants.LEFT_BACK_DRIVE, DrivetrainConstants.LEFT_BACK_TURN);
+  private final SwerveModule backRight = new SwerveModule(DrivetrainConstants.RIGHT_BACK_DRIVE, DrivetrainConstants.RIGHT_BACK_TURN);
+
+  private final  WPI_PigeonIMU pigeon = new WPI_PigeonIMU(DrivetrainConstants.PIGEON);
+
+  private final SwerveDriveKinematics kinematics =
+      new SwerveDriveKinematics(
+          frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+
+  private final SwerveDriveOdometry odometry =
+    new SwerveDriveOdometry(
+        kinematics,
+        pigeon.getRotation2d(),
+        new SwerveModulePosition[] {
+          frontLeft.getPosition(),
+          frontRight.getPosition(),
+          backLeft.getPosition(),
+          backRight.getPosition()
+        });
+
+  public DrivetrainSubsystem() {
+    pigeon.reset();
+  }
+
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    var swerveModuleStates =
+        kinematics.toSwerveModuleStates(
+            fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, pigeon.getRotation2d())
+                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, maxSpeed);
+    frontLeft.setDesiredState(swerveModuleStates[0]);
+    frontRight.setDesiredState(swerveModuleStates[1]);
+    backLeft.setDesiredState(swerveModuleStates[2]);
+    backRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  public void updateOdometry() {
+    odometry.update(
+        pigeon.getRotation2d(),
+        new SwerveModulePosition[] {
+          frontLeft.getPosition(),
+          frontRight.getPosition(),
+          backLeft.getPosition(),
+          backRight.getPosition()
+        });
+  }
 
   @Override
   public void periodic() {
