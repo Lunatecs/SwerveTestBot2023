fbe4d3a38fb2b5a90809edee524e801ae59641f2
Sujay Jukalkar <50773091+sujukal396@users.noreply.github.com>
2023-06-22 20:46:58 -0400

    Removed extra comments and cleanup of methods

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -31,87 +31,49 @@ public class SwerveModule {
     private String name;
     
     private static final double wheelRadius = 0.0508;
-    private static final double encoderResolution = 4096;//2048 for TalonFX
+    private static final double encoderResolution = 4096; // 2048 for TalonFX
 
     private static final double turnGearRatio = 150/7;
-    private static final double driveGearRatio = 6.75;//change later
+    private static final double driveGearRatio = 6.75; // TODO: change later
 
     //private static final double TICKS_PER_ROT = 2 * Math.PI * wheelRadius / encoderResolution;
     //private static final double TICKS_PER_RAD = 2 * Math.PI / encoderResolution;
 
     private static final double driveRot2Meter = Math.PI * 2 * wheelRadius / (encoderResolution * driveGearRatio);
-    private static final double turnRot2Rad = 2 * Math.PI / (encoderResolution * turnGearRatio); //note to self: gear ratios are divided
-    private static final double turnRot2Deg = 360 / (encoderResolution * turnGearRatio);
+    // private static final double turnRot2Rad = 2 * Math.PI / (encoderResolution * turnGearRatio);
+    private static final double turnRot2Rad = 2 * Math.PI / 360;
+    // private static final double turnRot2Deg = 360 / (encoderResolution * turnGearRatio);
     private static final double driveRPM2MPS = driveRot2Meter / 60;
-    private static final double turnRPM2RPS = turnRot2Rad / 60;
+    // private static final double turnRPM2RPS = turnRot2Rad / 60;
 
     private static final double moduleMaxAngularVelocity = DrivetrainSubsystem.maxAngularSpeed;
     private static final double moduleMaxAngularAcceleration = 2 * Math.PI;
 
-    CANCoderConfiguration config = new CANCoderConfiguration();
+    private final PIDController driveController = new PIDController(0.000001, 0, 0); // TODO: dummy values plz change later
 
-    public double driveMotorOutput;
-    public double turnMotorOutput;
+    // private final ProfiledPIDController turnController =
+    //     new ProfiledPIDController(0.5, 0, 0, new TrapezoidProfile.Constraints(moduleMaxAngularVelocity, moduleMaxAngularAcceleration));
 
-    //private final double absoluteEncoderOffsetRad;
-    //private final boolean absoluteEncoderReversed;
+    private PIDController turnController = new PIDController(1, 0.0, 0.0);
 
-    //canCoder.configAllSettings(config);
-    //Here, we ran into an issue where we were unable to use CANCoderConfiguration()
-    
-    private final PIDController driveController = new PIDController(0.000001, 0, 0); // dummy values plz change later
-
-    /* 
-    private final ProfiledPIDController turnController = new ProfiledPIDController(
-                                                                                .5, 
-                                                                                0,
-                                                                                0,
-                                                                                new TrapezoidProfile.Constraints(moduleMaxAngularVelocity, moduleMaxAngularAcceleration));
-    */
-
-    // no longer constant for reasons...
-    // let me cook
-    private PIDController turnController = new PIDController(.3, 0.0, 0.0);
-
-    private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(1, 3); // idk what happens here
-    private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(1, 0.5); // just trusting in wpilib
+    private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(1, 3);
+    private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(1, 0.5);
 
     public SwerveModule(int driveMotorID, int turnMotorID, int canCoderID, String name) {
         driveMotor = new TalonFX(driveMotorID);
         turnMotor = new TalonFX(turnMotorID);
         canCoder = new CANCoder(canCoderID);
-        //this.absoluteEncoderOffsetRad = absoluteEncoderOffsetRad;
-        //this.absoluteEncoderReversed = absoluteEncoderReversed;
-        this.name=name;
+        this.name = name;
 
-        /*
-         * wpi had code to set up encoders
-         * idk what we need to do for falcons
-         * that is a thursday problem
-         * - sujit
-         * 
-         * I think its allg, but im not sure
-         * - future sujit
-         * 
-         * lawd have mercy
-         * - more future sujit
-         * 
-         * Swerve is pain, CanCoders are messing everything up :(
-         * - Nikhil
-         */
-
-        canCoder.configFactoryDefault();
-
+        // canCoder.configFactoryDefault();
         driveMotor.configFactoryDefault();
-        turnMotor.configFactoryDefault();
-
-        
+        turnMotor.configFactoryDefault();       
 
         driveMotor.setNeutralMode(NeutralMode.Brake);
         turnMotor.setNeutralMode(NeutralMode.Brake);
 
-        turnMotor.configIntegratedSensorAbsoluteRange(AbsoluteSensorRange.Unsigned_0_to_360); // it didnt help :(
-        canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
+        // turnMotor.configIntegratedSensorAbsoluteRange(AbsoluteSensorRange.Unsigned_0_to_360);
+        // canCoder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
 
         //driveMotor.config_kP(0,.5);
         //driveMotor.config_kI(0, 0.0);
@@ -122,48 +84,22 @@ public class SwerveModule {
 
         turnController.enableContinuousInput(-Math.PI, Math.PI);
 
-
-
-        resetEncoders();
-    }
-
-    public void setPIDValues(double p, double i, double d) {// i hope it doesnt come to this
-        turnController = new PIDController(p, i, d);
-    }
-
-    private void resetEncoders() {
+        // Reset Encoders
         driveMotor.setSelectedSensorPosition(0);
-        turnMotor.setSelectedSensorPosition(0); //convert back to ticks
-                                                                                    //TODO: remember to use TODO next time please
-        //turnMotor.setSelectedSensorPosition(0);
-    
-    }
+        turnMotor.setSelectedSensorPosition(0);
 
-    public double getAbsoluteEncoderRad() {
-        return canCoder.getAbsolutePosition();// im not dealing with this rn - sujit
+        SmartDashboard.setDefaultNumber(name + " Turn Output", 0);
+        SmartDashboard.setDefaultNumber(name + " Sensor Position", 0);
+        
+        SmartDashboard.setDefaultNumber(name + " Measurement Desired", 0);
+        SmartDashboard.setDefaultNumber(name + " Measurement Actual", 0);
+        SmartDashboard.setDefaultNumber(name + " Setpoint", 0);
+        SmartDashboard.setDefaultNumber(name + " Turn Error", 0);
     }
     
     public SwerveModuleState getState() {
         // this code is adapted from the wpilib example code to work with falcons
         // will need to check if it actually works
-
-        // i used cancoder.getAbsolutePosition() and it may not be allg - sujit
-
-        //it might be good???
-
-        //it wasnt and still isnt good - still sujit
-        //some doo doo brain forgot to set turning motor encoder absolute range 
-        //(it was me)
-
-        //it still doesnt work dear god help
-
-        //lets go it works
-        //offsets still need working and pid is a little doo doo
-        //- very tired sujit (6/15)
-
-        //it stopped working when cancoders were used
-        //- eh (sujit)
-        //- nikhil too
 
         /* 
         SmartDashboard.putNumber("Drive State (Conversion) " + name, driveMotor.getSelectedSensorVelocity() * driveRPM2MPS);
@@ -172,7 +108,9 @@ public class SwerveModule {
         //SmartDashboard.putNumber("Turn State (Conversion) " + name, turnMotor.getSelectedSensorPosition() * turnRot2Deg);
         //SmartDashboard.putNumber("Turn State " + name, turnMotor.getSelectedSensorPosition());
 
-        return new SwerveModuleState(driveMotor.getSelectedSensorVelocity() * driveRPM2MPS, new Rotation2d(canCoder.getAbsolutePosition() * turnRot2Rad));
+        return new SwerveModuleState(driveMotor.getSelectedSensorVelocity() * driveRPM2MPS, new Rotation2d(
+                Math.toRadians(canCoder.getAbsolutePosition())
+            ));
     }
 
     public SwerveModulePosition getPosition() {
@@ -184,47 +122,55 @@ public class SwerveModule {
         //SmartDashboard.putNumber("Turn Position (Conversion) " + name, turnMotor.getSelectedSensorPosition() * turnRot2Rad);
         //SmartDashboard.putNumber("Turn Position " + name, turnMotor.getSelectedSensorPosition());
 
-        return new SwerveModulePosition(driveMotor.getSelectedSensorPosition() * driveRot2Meter, new Rotation2d(canCoder.getAbsolutePosition() * turnRot2Rad));
+        return new SwerveModulePosition(driveMotor.getSelectedSensorPosition() * driveRot2Meter, new Rotation2d(
+            Math.toRadians(canCoder.getAbsolutePosition())
+        ));
     }
 
-
+    public void periodic() {
+        SmartDashboard.putNumber(name + " Sensor Position", Math.toRadians(canCoder.getAbsolutePosition()));
+    }
 
     public void setDesiredState(SwerveModuleState desiredState) {
+        double driveMotorOutput;
+        double turnMotorOutput;
 
-        if (Math.abs(desiredState.speedMetersPerSecond) < 0.001) {
-            driveMotor.set(ControlMode.PercentOutput, 0);
-            turnMotor.set(ControlMode.PercentOutput, 0);
-            return;
-        }
+        // if (Math.abs(desiredState.speedMetersPerSecond) < 0.001) {
+        //     driveMotor.set(ControlMode.PercentOutput, 0);
+        //     turnMotor.set(ControlMode.PercentOutput, 0);
+        //     return;
+        // }
 
         // funi optimizaiton
         //SwerveModuleState state = SwerveModuleState.optimize(desiredState, new Rotation2d(turnMotor.getSelectedSensorPosition() * turnRot2Rad));
-        SwerveModuleState state = SwerveModuleState.optimize(desiredState, new Rotation2d(canCoder.getAbsolutePosition() * turnRot2Rad));
+        SwerveModuleState state = SwerveModuleState.optimize(desiredState, new Rotation2d(
+                Math.toRadians(canCoder.getAbsolutePosition())
+            ));
 
         //final double driveOutput = driveController.calculate(driveMotor.getSelectedSensorVelocity(), state.speedMetersPerSecond);
         //final double driveFF = driveFeedforward.calculate(state.speedMetersPerSecond);
-
-
-        final double turnOutput = turnController.calculate(canCoder.getAbsolutePosition() * turnRot2Rad, state.angle.getRadians());
+        
+        double turnEncoderPosition = (Math.toRadians(canCoder.getAbsolutePosition()) - Math.PI) * -1;
+        double turnOutput = turnController.calculate(turnEncoderPosition, state.angle.getRadians());
         //final double turnFF = turnFeedforward.calculate(turnController.getSetpoint().velocity);
 
-        driveMotorOutput = state.speedMetersPerSecond / DrivetrainSubsystem.maxSpeed;//13.0; //+ driveFF;
-        turnMotorOutput = turnOutput;//13.0; //+ turnFF;
+        // driveMotorOutput = state.speedMetersPerSecond / DrivetrainSubsystem.maxSpeed;//13.0; //+ driveFF;
+        // turnMotorOutput = turnOutput;//13.0; //+ turnFF;
 
-        SmartDashboard.putNumber("Turn Output " + name, turnMotorOutput);
-        //SmartDashboard.putNumber("Drive Output " + name, driveMotorOutput);
+        SmartDashboard.putNumber(name + " Turn Output", turnOutput);
+        //SmartDashboard.putNumber(name + " Drive Output", driveMotorOutput);
         
-        SmartDashboard.putNumber("Desired State: " + name, state.angle.getDegrees());
-        SmartDashboard.putNumber("Actual State: " + name, canCoder.getPosition() * turnRot2Deg);
-        SmartDashboard.putNumber("Setpoint: " + name, turnController.getSetpoint());
-        SmartDashboard.putNumber("Turn Error: " + name, turnController.getPositionError());
+        SmartDashboard.putNumber(name + " Measurement Desired", state.angle.getRadians());
+        SmartDashboard.putNumber(name + " Measurement Actual", turnEncoderPosition);
+        SmartDashboard.putNumber(name + " Setpoint", turnController.getSetpoint());
+        SmartDashboard.putNumber(name + " Turn Error", turnController.getPositionError());
 
         //SmartDashboard.putNumber(name + " Module Angle", getAngleDeg());
 
         // honestly, im just going off of wpilib
         //driveMotor.set(ControlMode.Current, 5.0);// driveoutput + driveff
         //driveMotor.set(ControlMode.PercentOutput, driveMotorOutput);
-        turnMotor.set(ControlMode.PercentOutput, turnMotorOutput);
+        turnMotor.set(ControlMode.PercentOutput, turnOutput);
         //turnMotor.set(ControlMode.Current, turnutput + turnFF);
 
         //SmartDashboard.putNumber("Drive Current Output " + name, this.driveMotor.getSupplyCurrent());
